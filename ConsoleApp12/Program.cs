9fe115e6296ce61e24eb067adfa33d7f82176eea
KarolinaAntonowicz <karola.antonowicz@gmail.com>
2019-11-30 21:24:57 +0100

    add new item to existing project

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp12
+namespace Appka_z_pierwszych_zajec
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+
+            while (true)
+            {
+                Console.WriteLine("1. Dodaj liczby");
+                Console.WriteLine("2. Odejmij liczby");
+                Console.WriteLine("3. *  liczby");
+                Console.WriteLine("4. Dziel  liczby");
+                Console.WriteLine("5. Wyjscie");
+                Console.WriteLine("6. niew")
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 6)
+
+                    break;
+                int b = Convert.ToInt32(Console.ReadLine());
+                int a = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 1)
+                {
+                    Console.WriteLine(a + b);
+                }
+            }
+
+
         }
     }
 }
